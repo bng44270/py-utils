2120181ec5f16c1b76bcb69b80cd444dec108d79
Andy Carlson <bng44270@gmail.com>
2023-12-21 16:44:46 -0600

    Update urlbuilder.py

--- urlbuilder.py
+++ urlbuilder.py
@@ -74,9 +74,19 @@ class UrlBuilder:
     self.ANCHOR = a if a[0] == '#' else ("#" + a)
   
   def __str__(self):
-    args = ("?" + "&".join([f"{k}={v}" for (k,v) in self.ARGS.items()])) if len(self.ARGS.items()) > 0 else ""
-    url = f"{self.PROTO}://{self.HOST}{self.PATH}{args}{self.ANCHOR}"
-    return url if self.___validurl() else "" 
+    if self.___validurl():
+      args = ("?" + "&".join([f"{k}={v}" for (k,v) in self.ARGS.items()])) if len(self.ARGS.items()) > 0 else ""
+      url = f"{self.PROTO}://{self.HOST}{self.PATH}{args}{self.ANCHOR}"
+      return url
+    elif self.___validpath():
+      args = ("?" + "&".join([f"{k}={v}" for (k,v) in self.ARGS.items()])) if len(self.ARGS.items()) > 0 else ""
+      url = f"{self.PATH}{args}{self.ANCHOR}"
+      return url
+    else:
+      return ""
+  
+  def ___validpath(self):
+    return (len(self.PATH) > 0)
   
   def ___validurl(self):
     return (len(self.PROTO) > 0 and len(self.HOST) > 0 and len(self.PATH) > 0)
@@ -89,18 +99,39 @@ class UrlBuilder:
       self.HOST = urlmatch.group("hostname")
       self.PATH = urlmatch.group("path")
     
-    argmatch = re.match("^[^:]+:\/\/[^\/]+[^?]+(?P<args>[?][^#]+)",url)
-    
-    # check for URL arguments (optional)
-    if argmatch:
-      argstr = argmatch.group("args")
-      if len(argstr):
-        argname = [a.split("=")[0] for a in re.sub(r"^\?","",argmatch.group("args")).split("&")]
-        argval = [url_decode(a.split("=")[1]) for a in re.sub(r"^\?","",argmatch.group("args")).split("&")]
-        self.ARGS = {k:v for (k,v) in zip(argname,argval)}
-    
-    anchormatch = re.match(r"^[^:]+:\/\/[^\/]+[^?]+\?[^#]+(?P<anchor>#.*)$",url)
-    
-    # check for anchor (optional)
-    if anchormatch:
-      self.ANCHOR = anchormatch.group("anchor")
+      argmatch = re.match("^[^:]+:\/\/[^\/]+[^?]+(?P<args>[?][^#]+)",url)
+      
+      # check for URL arguments (optional)
+      if argmatch:
+        argstr = argmatch.group("args")
+        if len(argstr):
+          argname = [a.split("=")[0] for a in re.sub(r"^\?","",argmatch.group("args")).split("&")]
+          argval = [url_decode(a.split("=")[1]) for a in re.sub(r"^\?","",argmatch.group("args")).split("&")]
+          self.ARGS = {k:v for (k,v) in zip(argname,argval)}
+      
+      anchormatch = re.match(r"^[^:]+:\/\/[^\/]+[^?]+\?[^#]+(?P<anchor>#.*)$",url)
+      
+      # check for anchor (optional)
+      if anchormatch:
+        self.ANCHOR = anchormatch.group("anchor")
+    else:
+      pathmatch = re.match(r"^(?P<path>\/[^?]+)",url)
+      
+      if pathmatch:
+        self.PATH = pathmatch.group("path")
+        
+        argmatch = re.match("^\/[^?]+(?P<args>[?][^#]+)",url)
+        
+        # check for URL arguments (optional)
+        if argmatch:
+          argstr = argmatch.group("args")
+          if len(argstr):
+            argname = [a.split("=")[0] for a in re.sub(r"^\?","",argmatch.group("args")).split("&")]
+            argval = [url_decode(a.split("=")[1]) for a in re.sub(r"^\?","",argmatch.group("args")).split("&")]
+            self.ARGS = {k:v for (k,v) in zip(argname,argval)}
+        
+        anchormatch = re.match(r"^\/[^?]+\?[^#]+(?P<anchor>#.*)$",url)
+        
+        # check for anchor (optional)
+        if anchormatch:
+          self.ANCHOR = anchormatch.group("anchor")
